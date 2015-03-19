21f33ca7154d1dce9f399b589293169fc0212363
Bhargava Shastry <bshastry@sec.t-labs.tu-berlin.de>
2015-03-19 23:57:56 +0100

    Switched to 64 bit cmake and ninja installs. Install prebuilt opt

--- scripts/fetch.sh
+++ scripts/fetch.sh
@@ -5,12 +5,13 @@ sudo patch -sp1 /usr/share/llvm-3.6/cmake/LLVMConfig.cmake < /vagrant/scripts/LL
 
 cd $HOME
 
-command -v cmake >/dev/null 2>&1 || { echo -e "\t[+] Installing Cmake"; wget -q http://www.cmake.org/files/v3.1/cmake-3.1.3-Linux-i386.tar.gz; sudo tar --strip-components=1 -xzf cmake-3.1.3-Linux-i386.tar.gz -C /usr/local; rm cmake-3.1.3-Linux-i386.tar.gz; }
+command -v cmake >/dev/null 2>&1 || { echo -e "\t[+] Installing Cmake"; wget -q http://www.cmake.org/files/v3.1/cmake-3.1.3-Linux-x86_64.tar.gz; sudo tar --strip-components=1 -xzf cmake-3.1.3-Linux-x86_64.tar.gz -C /usr/local; rm cmake-3.1.3-Linux-x86_64.tar.gz; }
 
 if [ ! -e depot_tools ]; then
 echo -e "\t[+] Installing depot tools"
 git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git &> /dev/null
 sudo update-alternatives --install /usr/bin/ninja ninja /home/vagrant/depot_tools/ninja 50 &> /dev/null
+sudo update-alternatives --install /usr/bin/ninja-linux64 ninja-linux64 /home/vagrant/depot_tools/ninja-linux64 50 &> /dev/null
 fi
 
 if [ -e demo ]; then
@@ -20,7 +21,7 @@ fi
 mkdir -p demo
 cd demo
 
-FETCH_FROM='https://owncloud.sec.t-labs.tu-berlin.de/owncloud/public.php?service=files&t=c3ec1d58809e9be6d1a03bd5e545d484&download'
+FETCH_FROM='https://owncloud.sec.t-labs.tu-berlin.de/owncloud/public.php?service=files&t=1b7c9e6f20d4cda3e45d18e9bbd1ec6a&download'
 
 # Fetch prebuilt stuff and demo material
 echo -e "\t[+] Fetching demo stuff. This is going to take a while..."
@@ -28,6 +29,10 @@ wget --no-check-certificate -qO demo.tar.gz $FETCH_FROM
 echo -e "\t[+] Extracting demo tarball"
 tar -zxf demo.tar.gz
 rm demo.tar.gz
+
+# Patch opt
+echo -e "\t[+] Installing pre-built opt 3.7 debug build"
+sudo update-alternatives --install /usr/bin/opt opt /home/vagrant/demo/prebuilt/clang-llvm/bin/opt 50 &> /dev/null
 
 # Fetch LLVM pass code
 if [ ! -e llvm-pass ]; then
